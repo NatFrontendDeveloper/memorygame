a1ec0a58a2f85dd1dfb94e1247d0173f86dc3707
NatFrontendDeveloper <105996297+NatFrontendDeveloper@users.noreply.github.com>
2024-12-23 22:13:45 +0100

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -51,23 +51,29 @@ document.addEventListener('DOMContentLoaded', () => {
 // Karten laden mittels AJAX
     // Загрузка карт через AJAX
     const loadCards = () => {
-        const selectedDeck = deckSelect.value; 
+    const selectedDeck = deckSelect.value; 
+    console.log('Selected deck:', selectedDeck); // Проверяем, что deckSelect работает корректно
+    const xhr = new XMLHttpRequest();
 
-        console.log(selectedDeck);
-        const xhr = new XMLHttpRequest();
-
-        xhr.open('GET', `/memorygame/assets/${selectedDeck}.json`, true);
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                const data = JSON.parse(xhr.responseText);
-                cards = createCards(data.cards, pairCount);
-                gameContainer.innerHTML = '';
-                startTime = Date.now();
-                displayCards(cards);
-            }
-        };
-        xhr.send();
+    xhr.open('GET', `/memorygame/assets/${selectedDeck}.json`, true);
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            cards = createCards(data.cards, pairCount);
+            gameContainer.innerHTML = '';
+            startTime = Date.now();
+            displayCards(cards);
+        } else {
+            console.error(`Ошибка загрузки файла: ${xhr.statusText}`);
+        }
     };
+    xhr.onerror = function () {
+        console.error('Ошибка при попытке загрузить файл');
+    };
+    xhr.send();
+};
+
+
 // Erstellen der Kartenpaare
     // Создание карт
     const createCards = (allCards, pairCount) => {
